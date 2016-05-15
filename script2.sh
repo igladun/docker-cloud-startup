22dc93eaf67ff1bdc281d68c993f84247e375927
Igor Gladun <igladun@accesssoftek.com>
2016-05-15 22:35:49 +0300

    Testing

--- script2.sh
+++ script2.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
-export DOCKERCLOUD_USER=$1
-export DOCKERCLOUD_APIKEY=$2
+export DOCKERCLOUD_USER="@docker_username@"
+export DOCKERCLOUD_APIKEY="@docker_apikey@"
+DOCKERCLOUD_ROLE="@dockercloud_environment@"
+DOCKERCLOUD_ENVIRONMENT="@dockercloud_environment@"
 
 DOCKER_CLOUD_CLI_VERSION="1.0.1"
-AWS_CLI_VERSION="1.9.20"
-
-
 
 apt-get update
 apt-get install -y python-pip jq
 
-pip install -q docker-cloud==$DOCKER_CLOUD_CLI_VERSION awscli==$AWS_CLI_VERSION
+pip install -q docker-cloud==$DOCKER_CLOUD_CLI_VERSION
 
 docker-cloud node byo | sed -n 4p | source /dev/stdin
 
 export NODE_UUID=$(cat /etc/dockercloud/agent/dockercloud-agent.conf | jq -r .UUID)
 echo $NODE_UUID
+
+docker-cloud tag add -t $DOCKERCLOUD_ROLE $NODE_UUID
+docker-cloud tag add -t $DOCKERCLOUD_ENVIRONMENT $NODE_UUID
 
 
 
